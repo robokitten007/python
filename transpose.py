7b8b4c5598beee738e2bd26d307ced685789cfdf
didicole <didicole@didicoles-air.myfiosgateway.com>
2024-09-25 17:14:45 -0400

    tansposing matrix

---
+++ transpose.py
@@ -0,0 +1,13 @@
+
+
+
+matrix = [[1, 2, 3],[4, 5, 6]]
+
+transpose = []
+for col in range(len(matrix[0])):
+    new_row = []
+    for row in range(len(matrix)):
+        new_row.append(0)
+    transpose.append(new_row)
+
+print(transpose)
